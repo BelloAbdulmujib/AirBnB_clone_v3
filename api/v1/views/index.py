f102da69c4717ecf605f881abc5feb0f28454e98
Mariam <kelanimaryam1234@gmail.com>
2024-05-02 09:55:55 -0700

    index.py file  updated

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,15 +8,14 @@ from flask import jsonify
 from models import storage
 
 
-@app_views.route("/status", methods=['GET'])
-def status():
-    """ function of status """
-    return jsonify({"status": "OK"})
+@app_views.route('/status', strict_slashes=False)
+def my_app_status():
+    return (jsonify({"status": "OK"}))
 
 
-@app_views.route("/status", methods=['GET'])
-def all_state():
-    """ all json obj's returned """
+@app_views.route("/stats", strict_slashes=False)
+def all_the_stats():
+    """ all json obects in the file are returned """
     class_object = {
         "amenities": storage.count("Amenities"),
         "cities": storage.count("Cities"),
@@ -25,4 +24,4 @@ def all_state():
         "states": storage.count("States"),
         "users": storage.count("Users")
     }
-    return jsonify(class_object)
+    return (jsonify(class_object))
