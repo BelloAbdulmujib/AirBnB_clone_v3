0dee41c755d938549231231bd14920c1f680aa8c
BelloAbdulmujib <belloabdulmujib3@gmail.com>
2024-05-02 16:11:28 -1100

    AirBnB_clone_v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,11 +10,23 @@ from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
+def teardown(Exception):
+    """ function of teardown """
     storage.close()
+
+
+@app.errorhandler(404)
+def detect_issue(error):
+    """ detects the issue """
+    response = show_response(jsonify({'error': 'Not found'}), 404)
+    return response
+
 
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python3
 """
-creating a rout that returns a JSON obj
+creating a rout that returns a JSON
 """
-from flask import jsonify
-from api.v1.views import app_views
 
-@app_views.route('/status', strict_slashes=False)
-"""view point that return json output"""
-def app_status():
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+
+@app_views.route("/status", methods=['GET'])
+def status():
+    """ func of status """
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/status", methods=['GET'])
+def all_state():
+    """ shows the objects after retrieval """
+    class_object = {
+        "amenities": storage.count("Amenities"),
+        "cities": storage.count("Cities"),
+        "places": storage.count("Places"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("States"),
+        "users": storage.count("Users")
+    }
+    return jsonify(class_object)
