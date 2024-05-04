4c91ea4f9665bcd7d46cb557a23f219617b64019
Mariam <kelanimaryam1234@gmail.com>
2024-05-04 00:49:43 -0700

    edited sates.py file

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,7 +4,8 @@ from models.state import State
 from api.v1.views import app_views
 
 @app_views.route('/states', methods=['GET'])
-def get_states():
+def dic_states():
+    """Returns the dictionary of States in state.py"""
     dico = []
     for ind in storage.all(State).values():
         dico.append(val.to_dict())
@@ -12,6 +13,7 @@ def get_states():
 
 @app_views.route('/states/<path:state_id>')
 def get_state(state_id):
+    """Gets all the elements of State from states.py"""
     state = storage.get(State, state_id)
     if state is None:
         abort(404)
