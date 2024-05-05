59e765d4e286128b225b16620144ca86524dcbb1
Mariam <kelanimaryam1234@gmail.com>
2024-05-05 03:58:01 -0700

    states.py file

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -26,8 +26,7 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<path:state_id>', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/states/<path:state_id>', methods=['DELETE'])
 def delete_state(state_id):
     """delete the state element"""
     state = storage.get(State, state_id)
